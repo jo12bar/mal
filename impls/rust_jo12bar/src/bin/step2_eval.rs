2c7624e558d683b8ff5d6d7f6d82354a2744997d
Johann Barnard <johann.b@telus.net>
2020-05-29 17:21:50 -0700

    Implement basic evaluation and binary numeric operators.

---
+++ impls/rust_jo12bar/src/bin/step2_eval.rs
@@ -0,0 +1,178 @@
+use mal_rust_jo12bar::{
+    printer::pr_str,
+    reader::read_line,
+    readline::Readline,
+    types::{Atom, Error, Expr, HashMapKey},
+};
+use std::collections::HashMap;
+
+/// This is the Lisp environment that holds all built-in functions.
+type Env<'a> = HashMap<HashMapKey, Expr<'a>>;
+
+/// This represents a two-argument `Expr::Constant(Atom::Int)` **OR**
+/// `Expr::Constant(Atom::Float)` operation.
+///
+/// `int_op` and `float_op` will usually do the same thing, but for typing
+/// reasons you may have to explicitly rewrite them twice.
+fn binary_num_op(
+    int_op: impl Fn(i64, i64) -> i64,
+    float_op: impl Fn(f64, f64) -> f64,
+    args: Vec<Expr>,
+) -> Result<Expr, Error> {
+    if args.len() != 2 {
+        return Err(Error::Str(format!(
+            "Wrong number of arguments given to binary integer operator.\n\
+             Expected 2 arguments, found {}.",
+            args.len()
+        )));
+    }
+
+    match (args[0].clone(), args[1].clone()) {
+        (Expr::Constant(Atom::Int(a)), Expr::Constant(Atom::Int(b))) => {
+            Ok(Expr::Constant(Atom::Int(int_op(a, b))))
+        }
+
+        (Expr::Constant(Atom::Float(a)), Expr::Constant(Atom::Float(b))) => {
+            Ok(Expr::Constant(Atom::Float(float_op(a, b))))
+        }
+
+        _ => Err(Error::Str(
+            "Arguments passed to binary numeric operator are of invalid type.".to_string(),
+        )),
+    }
+}
+
+/// Evaluates a single sub-section of the AST.
+fn eval_ast<'a>(ast: &'a Expr, env: &'a Env) -> Result<Expr<'a>, Error<'a>> {
+    match ast {
+        Expr::Constant(Atom::Sym(sym)) => {
+            // Look up symbol in `env`, and return its associated value if found.
+            // If not found, raise an error.
+            if let Some(func) = env.get(&HashMapKey::Sym(sym.clone())) {
+                Ok(func.clone())
+            } else {
+                Err(Error::Str(format!("Unknown symbol: {}", sym)))
+            }
+        }
+
+        Expr::List(exprs) => {
+            // Return a new list that is the result of calling `eval()` on each
+            // member of the original list.
+            let mut evaluated_exprs = Vec::with_capacity(exprs.len());
+
+            for expr in exprs.iter() {
+                evaluated_exprs.push(eval(expr, env)?);
+            }
+
+            Ok(Expr::List(evaluated_exprs))
+        }
+
+        Expr::Vec(exprs) => {
+            // Return a new vector that is the result of calling `eval()` on
+            // each element of the original vector.
+            let mut evaluated_exprs = Vec::with_capacity(exprs.len());
+
+            for expr in exprs.iter() {
+                evaluated_exprs.push(eval(expr, env)?);
+            }
+
+            Ok(Expr::Vec(evaluated_exprs))
+        }
+
+        Expr::HashMap(hmap) => {
+            // Return a new hash-map, where:
+            //
+            // - Each each key is the same key as from the original hash-map
+            // - Each value is the result of calling `eval()` on the original
+            //   hash-map's corresponding value.
+            let mut new_hashmap = HashMap::with_capacity(hmap.len());
+
+            for (k, v) in hmap.iter() {
+                new_hashmap.insert(k.clone(), eval(v, env)?);
+            }
+
+            Ok(Expr::HashMap(new_hashmap))
+        }
+
+        _ => Ok(ast.clone()),
+    }
+}
+
+/// Evaluates an expression.
+fn eval<'a>(ast: &'a Expr, env: &'a Env) -> Result<Expr<'a>, Error<'a>> {
+    match ast {
+        // If `ast` is a list, then we evaluate it.
+        Expr::List(exprs) => {
+            // If the list is empty, just return `ast` unchanged.
+            if exprs.is_empty() {
+                Ok(ast.clone())
+            } else {
+                // Otherwise, call `eval_ast` to get a new evaluated list.
+                match eval_ast(ast, env)? {
+                    Expr::List(new_exprs) => {
+                        let func = new_exprs.clone().first().unwrap().clone();
+                        func.apply(new_exprs[1..].to_vec())
+                    }
+
+                    other => Err(Error::Str(format!(
+                        "Expected a list when evaluating a list.\nGot {}",
+                        other
+                    ))),
+                }
+            }
+        }
+
+        // If `ast` is *not* a `Expr::List`, then return the result of calling
+        // `eval_ast` on it.
+        other_expr => eval_ast(other_expr, env),
+    }
+}
+
+/// The main REPL.
+fn repl(env: &Env) {
+    let mut readline = Readline::new("mal> ");
+
+    while let Some(line) = readline.get() {
+        // For now, just print the line back out.
+        if !line.is_empty() {
+            match read_line(&line.as_str()) {
+                Ok(expr) => match eval(&expr, env) {
+                    Ok(result) => pr_str(result),
+                    Err(error) => eprintln!("{}", error),
+                },
+                Err(e) => eprintln!("{}", e),
+            }
+
+            // mal_rust_jo12bar::reader::parse_line_and_print_ast(&line.as_str());
+        }
+    }
+
+    // Save the history at the end.
+    readline.save_history();
+}
+
+fn main() {
+    let mut builtin_env = Env::with_capacity(4);
+
+    builtin_env.insert(
+        HashMapKey::Sym("+".to_string()),
+        Expr::func(|args| binary_num_op(|a, b| a + b, |a, b| a + b, args)),
+    );
+
+    builtin_env.insert(
+        HashMapKey::Sym("-".to_string()),
+        Expr::func(|args| binary_num_op(|a, b| a - b, |a, b| a - b, args)),
+    );
+
+    builtin_env.insert(
+        HashMapKey::Sym("*".to_string()),
+        Expr::func(|args| binary_num_op(|a, b| a * b, |a, b| a * b, args)),
+    );
+
+    builtin_env.insert(
+        HashMapKey::Sym("/".to_string()),
+        Expr::func(|args| binary_num_op(|a, b| a / b, |a, b| a / b, args)),
+    );
+
+    repl(&builtin_env);
+}
--- impls/rust_jo12bar/src/types.rs
+++ impls/rust_jo12bar/src/types.rs
@@ -1,10 +1,29 @@
 //! MAL types.
 
 use std::collections::HashMap;
+use std::rc::Rc;
+
+/// Some error types.
+#[derive(PartialEq, Clone, Debug)]
+pub enum Error<'a> {
+    /// For errors where the message is some arbritrary `String`.
+    Str(String),
+    /// For errors where the message is to involve an `Atom`.
+    Atom(Atom<'a>),
+}
+
+impl std::fmt::Display for Error<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Str(s) => s.fmt(f),
+            Self::Atom(a) => a.fmt(f),
+        }
+    }
+}
 
 /// Primitive values.
-#[derive(Debug, PartialEq, Clone)]
-pub enum Atom {
+#[derive(Clone)]
+pub enum Atom<'a> {
     Nil,
     Str(String),
     Bool(bool),
@@ -12,9 +31,33 @@ pub enum Atom {
     Float(f64),
     Sym(String),
     Keyword(String),
+
+    /// First field is the function itself, while the second field can be used
+    /// for its name.
+    Func(Rc<dyn Fn(Vec<Expr>) -> Result<Expr, Error> + 'a>),
 }
 
-impl std::fmt::Display for Atom {
+impl PartialEq for Atom<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Nil, Self::Nil) => true,
+            (Self::Str(ref a), Self::Str(ref b)) => a == b,
+            (Self::Bool(ref a), Self::Bool(ref b)) => a == b,
+            (Self::Int(ref a), Self::Int(ref b)) => a == b,
+            (Self::Float(ref a), Self::Float(ref b)) => a == b,
+            (Self::Sym(ref a), Self::Sym(ref b)) => a == b,
+            (Self::Keyword(ref a), Self::Keyword(ref b)) => a == b,
+
+            // Two functions can never equal each other.
+            (Self::Func(..), Self::Func(..)) => false,
+
+            // Two atoms of differing typw cannot equal each other.
+            _ => false,
+        }
+    }
+}
+
+impl std::fmt::Display for Atom<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Nil => write!(f, "nil"),
@@ -24,6 +67,26 @@ impl std::fmt::Display for Atom {
             Self::Float(fl) => fl.fmt(f),
             Self::Sym(sym) => sym.fmt(f),
             Self::Keyword(kwd) => write!(f, ":{}", kwd),
+            Self::Func(func) => write!(f, "#<fn at {:p}>", func),
+        }
+    }
+}
+
+impl std::fmt::Debug for Atom<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Nil => write!(f, "Nil"),
+            Self::Str(s) => write!(f, "Str({:?})", s),
+            Self::Bool(b) => write!(f, "Bool({:?})", b),
+            Self::Int(i) => write!(f, "Int({:?})", i),
+            Self::Float(fl) => write!(f, "Float({:?})", fl),
+            Self::Sym(sym) => write!(f, "Sym({:?})", sym),
+            Self::Keyword(kwd) => write!(f, "Keyword({:?})", kwd),
+            Self::Func(func) => write!(
+                f,
+                "Func(Rc<dyn Fn(Vec<Expr>) -> Result<Expr, Error>> at {:p})",
+                func
+            ),
         }
     }
 }
@@ -34,9 +97,12 @@ pub enum HashMapKey {
     Str(String),
     Keyword(String),
     Int(i64),
+    /// Symbols as `HashMapKey`s should be for internal, non-user-accessible use
+    /// only (i.e. not part of MAL itself).
+    Sym(String),
 }
 
-impl std::convert::TryFrom<Atom> for HashMapKey {
+impl std::convert::TryFrom<Atom<'_>> for HashMapKey {
     type Error = String;
 
     fn try_from(atom: Atom) -> Result<Self, Self::Error> {
@@ -44,6 +110,7 @@ impl std::convert::TryFrom<Atom> for HashMapKey {
             Atom::Str(s) => Ok(Self::Str(s)),
             Atom::Keyword(k) => Ok(Self::Keyword(k)),
             Atom::Int(i) => Ok(Self::Int(i)),
+            Atom::Sym(s) => Ok(Self::Sym(s)),
             other_atom_type => Err(format!(
                 "Cannot use {:?} as a hash-map key!",
                 other_atom_type
@@ -58,6 +125,7 @@ impl std::fmt::Display for HashMapKey {
             Self::Str(s) => write!(f, "\"{}\"", s),
             Self::Keyword(k) => write!(f, ":{}", k),
             Self::Int(i) => i.fmt(f),
+            Self::Sym(sym) => write!(f, "{}", sym),
         }
     }
 }
@@ -65,15 +133,15 @@ impl std::fmt::Display for HashMapKey {
 /// An expression. Could just be a single `Atom`, or it could be something like
 /// a list or a function invocation.
 #[derive(Debug, PartialEq, Clone)]
-pub enum Expr {
+pub enum Expr<'a> {
     Comment,
-    Constant(Atom),
-    List(Vec<Expr>),
-    Vec(Vec<Expr>),
-    HashMap(HashMap<HashMapKey, Expr>),
+    Constant(Atom<'a>),
+    List(Vec<Expr<'a>>),
+    Vec(Vec<Expr<'a>>),
+    HashMap(HashMap<HashMapKey, Expr<'a>>),
 }
 
-impl Expr {
+impl<'a> Expr<'a> {
     /// A "reader macro." Wraps an `Expr` like this:
     ///
     /// | Input   | Output         |
@@ -137,9 +205,27 @@ impl Expr {
             expr1,
         ])
     }
+
+    /// Creates a new `Atom::Func` wrapped in a `Expr::Constant`.
+    pub fn func<F>(f: F) -> Self
+    where
+        F: Fn(Vec<Expr>) -> Result<Expr, Error> + 'a,
+    {
+        Self::Constant(Atom::Func(Rc::new(f)))
+    }
+
+    /// Apply arguments to a function. Will return an `Error` if you attempt to
+    /// call a non-function `Expr`.
+    pub fn apply<'b>(&'a self, args: Vec<Expr<'b>>) -> Result<Expr<'b>, Error<'b>> {
+        match self {
+            Self::Constant(Atom::Func(f)) => f(args),
+
+            _ => Err(Error::Str("Attempt to call a non-function!".to_string())),
+        }
+    }
 }
 
-impl std::fmt::Display for Expr {
+impl std::fmt::Display for Expr<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Comment => write!(f, ""),
